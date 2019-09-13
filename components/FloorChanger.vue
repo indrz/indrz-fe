a260263fb939582009979d098a5c17c00e7819c6
titu <email.to.titu@gmail.com>
2019-09-13 12:18:21 +0600

    #8 Component: Floor changer

---
+++ components/FloorChanger.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card
+    class="mx-auto position"
+  >
+    <v-list dense>
+      <v-list-item-group v-model="model" mandatory color="primary">
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      model: 1,
+      items: [
+        {
+          text: '1'
+        },
+        {
+          text: '2'
+        },
+        {
+          text: '3'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .position {
+    position: absolute;
+    right: 10px;
+    top: 70px;
+  }
+</style>
