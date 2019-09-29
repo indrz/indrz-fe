27a55245a7fc54bdeff7fd6712da4d49db0aff77
mdiener21 <mdiener21@gmail.com>
2019-09-29 11:09:53 +0200

    eslint fix

--- util/indrzConfig.js
+++ util/indrzConfig.js
@@ -1,8 +1,8 @@
 export default {
-  baseApiUrl: 'http://localhost:8000/api/v1/',
+  baseApiUrl: 'https://tu.indrz.com' + '/api/v1/',
   defaultCenterXY: [1822252.75, 6139984.70],
   baseWmsUrl: 'https://www.indrz.com/geoserver/wms',
-  searchUrl: 'https://localhost/en/search',
+  searchUrl: this.baseApiUrl + '/search',
   token: 'Token 42519ebe7bada4d7a151c76832b94614ea5b198d',
   layerNamePrefix: 'floor_',
   geoServerLayerPrefix: 'indrztu:'
