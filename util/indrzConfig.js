64821d0310d69ecbfb983dbaec72cfdae3ce8e7f
Titu <email.to.titu@gmail.com>
2019-09-30 11:04:33 +0600

    fixed scoping issue

--- util/indrzConfig.js
+++ util/indrzConfig.js
@@ -1,8 +1,10 @@
+const baseApiUrl = 'https://tu.indrz.com/api/v1/';
+
 export default {
-  baseApiUrl: 'https://tu.indrz.com' + '/api/v1/',
+  baseApiUrl,
   defaultCenterXY: [1822252.75, 6139984.70],
   baseWmsUrl: 'https://www.indrz.com/geoserver/wms',
-  searchUrl: this.baseApiUrl + '/search',
+  searchUrl: baseApiUrl + '/search',
   token: 'Token 42519ebe7bada4d7a151c76832b94614ea5b198d',
   layerNamePrefix: 'floor_',
   geoServerLayerPrefix: 'indrztu:'
