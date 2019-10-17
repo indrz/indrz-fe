5b77068c7363a150385e91d7bc60529354d1ed5d
mdiener21 <mdiener21@gmail.com>
2019-10-17 09:05:20 +0200

    update zoom to campus zoom level

--- components/IndrzMap.vue
+++ components/IndrzMap.vue
@@ -312,7 +312,7 @@ export default {
       this.view.animate({
         center: centroid.coordinates,
         duration: 2000,
-        zoom: 17
+        zoom: 16
       });
     },
     onFloorClick (floorName) {
