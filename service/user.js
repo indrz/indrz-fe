1e9cd26c26af0f2b2834beb8bde692f3a76e591c
Titu <email.to.titu@gmail.com>
2019-11-22 10:43:33 +0600

    #80 - login using api endpoint response

--- service/user.js
+++ service/user.js
@@ -1,10 +1,18 @@
+import Api from '../util/api';
+
 const signIn = (userInfo) => {
+  return Api.postRequest({
+    endPoint: 'api-token-auth/',
+    data: userInfo
+  });
+  /*
   return {
     data: {
       userName: userInfo.username,
       token: 'dummy-token'
     }
   };
+  */
 };
 
 export default {
