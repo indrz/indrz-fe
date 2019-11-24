e9dc6a317de3ab9dcbb5b47b1c4d08f4ad1f19b3
mdiener21 <mdiener21@gmail.com>
2019-11-24 11:49:50 +0100

    poi icon path updated to use icon property

--- components/poi/PointsOfInterest.vue
+++ components/poi/PointsOfInterest.vue
@@ -27,8 +27,8 @@
         </div>
       </template>
       <template v-slot:prepend="{ item, open, active }">
-        <img v-if="active" :src="'/images/icons/' + item.icon + '_active_24.png'" @click="onTreeClick(item)">
-        <img v-else :src="'/images/icons/' + item.icon + '_24.png'" @click="onTreeClick(item)">
+        <img v-if="active" :src="item.icon" @click="onTreeClick(item)">
+        <img v-else :src="item.icon" @click="onTreeClick(item)">
         <v-icon v-if="!item.icon">
           {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
         </v-icon>
