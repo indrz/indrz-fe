0f1c8d2a854c87ab4a2bf053ea87770792168287
Titu <email.to.titu@gmail.com>
2019-12-05 17:52:27 +0600

    #90 - WIP: added the required property

--- components/admin/poi/PoiManager.vue
+++ components/admin/poi/PoiManager.vue
@@ -129,7 +129,8 @@ export default {
     async deletePoi (selectedPoi) {
       await api.postRequest({
         endPoint: `poi/${selectedPoi.featureId}`,
-        method: 'DELETE'
+        method: 'DELETE',
+        data: {}
       });
     },
     onCancelButtonClick () {
