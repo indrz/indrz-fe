6a70eb2837d0bc13a82b5a3afa7215f2dae11940
mdiener21 <mdiener21@gmail.com>
2020-06-22 11:27:43 +0200

    added config for urls

--- util/indrzConfig.js
+++ util/indrzConfig.js
@@ -1,8 +1,13 @@
 const baseUrl = 'https://tuw-maps.tuwien.ac.at';
 const baseApiUrl = baseUrl + '/api/v1/'
+const homepageUrl = 'https://tuwien.at';
+const leftMenuLogo = '/images/tu-logo.png';
+
 
 export default {
   baseApiUrl,
+  homepageUrl,
+  leftMenuLogo,
   defaultCenterXY: [1823820.8003225543,6138685.150457315], // TU
   baseWmsUrl: 'https://tuw-maps.tuwien.ac.at/geoserver/wms',
   searchUrl: baseApiUrl + 'search',
