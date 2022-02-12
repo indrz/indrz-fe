4724fcd914632af872a0626c864de55aeaa93751
Michael <mdiener21@gmail.com>
2022-02-12 12:50:49 +0100

    removed zoom to full extent of all poi's on activate

--- util/mapHandler.js
+++ util/mapHandler.js
@@ -400,11 +400,6 @@ const handlePoiLoad = (map, activeFloorNum, { removedItems, newItems, oldItems }
                   layer.getLayers().push(poiLayer);
                 }
               });
-
-              map.getView().fit(poiLayer.getSource().getExtent(), {
-                maxZoom: 18,
-                duration: 500
-              });
             });
         }
       }
