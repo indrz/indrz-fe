a4d5c47eac43dc0a3542a274b172b85b32d04606
michael diener <mdiener21@gmail.com>
2023-08-02 12:41:48 +0000

    set nuxt conf to use config env.TITLE

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: process.env.TITLE || '',
+    title: process.env.TITLE || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
