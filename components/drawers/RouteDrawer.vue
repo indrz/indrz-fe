341a77affd476f67dcbed898980c9f9130cafc5e
mdiener21 <mdiener21@gmail.com>
2023-10-29 20:28:13 +0100

    added space_type to cover option to identify space

--- components/drawers/RouteDrawer.vue
+++ components/drawers/RouteDrawer.vue
@@ -235,7 +235,7 @@ export default {
 
       if (!properties.space_id && properties.spaceid) {
         properties.space_id = properties.spaceid;
-      } else if (properties?.src_icon === 'space' || properties?.space_type_id) {
+      } else if (properties?.src_icon === 'space' || properties?.space_type_id || properties?.space_type) {
         properties.space_id = id;
       }
 
@@ -297,7 +297,7 @@ export default {
       if (!routeData.name && routeData.room_code) {
         routeData.name = routeData.room_code;
       }
-      if (!routeData.space_id && (routeData?.src_icon === 'space' || routeData?.space_type_id)) {
+      if (!routeData.space_id && (routeData?.src_icon === 'space' || routeData?.space_type_id || routeData?.space_type)) {
         routeData.space_id = routeData.id;
       }
       const data = {
