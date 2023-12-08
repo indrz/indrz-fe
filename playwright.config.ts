be420058920afdce7dfe50f6bf1a4fe3b943244e
Nick Dare <nick.j.dare@gmail.com>
2023-12-08 20:30:34 +1100

    ci: add build and playwright test build steps

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,12 +71,12 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' }
     // }
-  ]
+  ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'yarn run dev',
+    url: 'http://localhost:3000/',
+    reuseExistingServer: !process.env.CI,
+  },
 });
